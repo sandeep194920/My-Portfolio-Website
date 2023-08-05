309efd29f7e9d9176afe4d2ea4731a8a46228fdb
sandeep194920 <sandeepamaranath@gmail.com>
2023-08-05 18:40:07 -0400

    minor fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,16 @@ import { useEffect } from 'react'
 import AOS from 'aos'
 
 import 'aos/dist/aos.css'
+import Head from 'next/head'
 export default function Home() {
   useEffect(() => {
     AOS.init()
   }, [])
   return (
     <Fragment>
+      <Head>
+        <title>Sandeep Amarnath</title>
+      </Head>
       <Header />
       <Body />
     </Fragment>
